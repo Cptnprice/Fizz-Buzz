7b6a84f1d0af45b3364a55d4ff255e6d738b0ba2
shalva chkuaselidze <shalvachkuaselidze77@gmail.com>
2019-08-19 19:45:24 +0400

    Delete code.py

--- code.py
+++
@@ -1,1 +0,0 @@
-fizz_buzz = lambda n : ['Fizz','Buzz','Fizz Buzz','{}'.format(n)][{n:3,15:2,3:0,5:1}[[(n % i, i) for i in [15,3,5]].__add__([(0,n)])[[n % i for i in [15,3,5]].__add__([0]).index(0)][1]]]
